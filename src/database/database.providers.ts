c617addb5b68b2320ffed52155c7776956cf2c7c
Dylan-Chambi <dylanchambifi@gmail.com>
2023-10-15 00:18:03 -0500

    Docker compose file and setting environment variables

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,13 +6,16 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'test',
+        host: process.env.MYSQL_HOST,
+        port: parseInt(process.env.MYSQL_PORT),
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
         entities: [__dirname + '/../**/*.entity{.ts,.js}',],
         synchronize: true,
+        extra: {
+          connectionTimeoutMillis: 5000,
+        }
       });
 
       return dataSource.initialize();
